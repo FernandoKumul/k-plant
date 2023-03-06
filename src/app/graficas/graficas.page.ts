ef71c07ae504ad11f8f0e0d98dbdf65146fb72b6
Ana <moralesgarciaanateresa@gmail.com>
2023-03-06 07:40:29 -0500

    Grafica parte2

--- src/app/graficas/graficas.page.ts
+++ src/app/graficas/graficas.page.ts
@@ -22,26 +22,64 @@ export class GraficasPage implements AfterViewInit {
   }
 
   doughnutChartMethod(){
-    this.doughnurtChart = new Chart(this.doughnutCanvas.nativeElement, {
-      type:'doughnut',
+    this.doughnutChart = new 
+    Chart(this.doughnutCanvas.nativeElement, {
+      type: 'doughnut',
       data: {
-        labels: []
-        data: [50, 29, 15, 10, 7],
-        backgroundColor:[
-
-        ]
+        labels: ['BJP', 'Congress', 'AAP', 'CPM', 'SP'],
+        datasets: [{
+          label: '# of Votes',
+          data: [50, 29, 15, 10, 7],
+          backgroundColor: [
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)'
+          ],
+          hoverBackgroundColor: [
+            '#FFCE56',
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56',
+            '#FF6384'
+          ]
+        }]
       }
-
-
-    }
-
+    });
   }
 
-
-
-  ngOnInit() {
+  lineChartMethod() {
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+      type: 'line',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'November', 'December'],
+        datasets: [
+          {
+            label: 'Sell per week',
+            fill: false,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40, 10, 5, 50, 10, 15],
+            spanGaps: false,
+          }
+        ]
+      }
+    });
   }
 
 
 }
- 
