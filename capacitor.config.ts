6d8980dcfdbc8f56cd2cc3afe703203565564744
PoquetzSY <luisgomez7829@gmail.com>
2023-03-15 02:27:35 -0500

    Splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,9 +2,27 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Kapa',
+  appName: 'K-Plant',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins:{
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#fff8e6",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      // androidSpinnerStyle: "large",
+      // iosSpinnerStyle: "small",
+      // spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      // layoutName: "launch_screen",
+      // useDialog: true,
+    },
+  }
 };
 
 export default config;
